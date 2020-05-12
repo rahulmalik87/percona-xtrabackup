c9b69142eb755c561c8d827e0208f7d3b24064f5
Rahul Malik <rahul.malik@percona.com>
2020-05-12 11:21:55 +0530

    fixed bug1130627.sh

--- storage/innobase/xtrabackup/test/t/bug1130627.sh
+++ storage/innobase/xtrabackup/test/t/bug1130627.sh
@@ -24,7 +24,7 @@ EOF
 force_checkpoint
 
 # Test that specifying partitions with --tables works
-xtrabackup --backup --tables='^test.*#p#p5' --target-dir=$topdir/backup
+xtrabackup --backup --tables='^test.*#p5' --target-dir=$topdir/backup
 xtrabackup --prepare --target-dir=$topdir/backup
 
 file_cnt=`ls $topdir/backup/test | wc -l`
@@ -38,7 +38,7 @@ rm -rf $topdir/backup
 # Test that specifying partitions with --databases works
 
 xtrabackup --backup \
-    --databases='test.t_innodb#p#p5' --target-dir=$topdir/backup
+    --databases='test.t_innodb#p#p5 test.t_innodb#P#p5' --target-dir=$topdir/backup
 xtrabackup --prepare --target-dir=$topdir/backup
 
 test "$file_cnt" -eq 1
@@ -49,6 +49,7 @@ rm -rf $topdir/backup
 # Test that specifying partitions with --tables-file works
 cat >$topdir/tables_file <<EOF
 test.t_innodb#p#p5
+test.t_innodb#P#p5
 EOF
 xtrabackup --backup --tables-file=$topdir/tables_file --target-dir=$topdir/backup
 xtrabackup --prepare --target-dir=$topdir/backup
