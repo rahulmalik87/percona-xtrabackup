7c1cf374a50edd8714cd5c69d1ac7f5eeca5f81f
Rahul Malik <rahul.malik@percona.com>
2020-11-03 15:27:13 +0530

    test patch mlog file extend

--- storage/innobase/fil/fil0fil.cc
+++ storage/innobase/fil/fil0fil.cc
@@ -10890,7 +10890,7 @@ byte *fil_tablespace_redo_extend(byte *ptr, const byte *end,
     return ptr;
   }
 
-#ifndef UNIV_HOTBACKUP
+#ifndef XTRABACKUP
   const auto result =
       fil_system->get_scanned_filename_by_space_id(page_id.space());
 
@@ -11006,7 +11006,7 @@ byte *fil_tablespace_redo_extend(byte *ptr, const byte *end,
   fil_flush(space->id);
 
   fil_space_close(space->id);
-#endif /* !UNIV_HOTBACKUP */
+#endif /* !XTRABACKUP*/
 
   return ptr;
 }
